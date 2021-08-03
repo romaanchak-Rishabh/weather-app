f9b28e88415bb127c637219931d21c1dafaffc3d
Rishabh Tater <rishabhtater14@gmail.com>
2021-08-03 23:14:16 +0530

    Updates

---
+++ precache-manifest.c99f367773387195eab268e4f9806e04.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51f2c515e936e58ef9970aaff7e2c372",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "8bede978349dd5f01791",
+    "url": "/weather-app/static/css/main.505a7a61.chunk.css"
+  },
+  {
+    "revision": "a2040c8733dc2519f173",
+    "url": "/weather-app/static/js/2.9ad2e7d3.chunk.js"
+  },
+  {
+    "revision": "8bede978349dd5f01791",
+    "url": "/weather-app/static/js/main.ff342d11.chunk.js"
+  },
+  {
+    "revision": "2a6542eb0b34daff9ee0",
+    "url": "/weather-app/static/js/runtime-main.5ece7d69.js"
+  }
+]);
